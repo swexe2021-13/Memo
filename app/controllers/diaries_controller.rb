38a9dce878dbf1de9a1f83978781cf19d584f8fa
Chisato Morita <1911140002c@ed.fuk.kindai.ac.jp>
2021-11-08 11:04:21 +0000

    aaa

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -9,8 +9,30 @@ class DiariesController < ApplicationController
     end
     
     def create
-        diary = Diary.new(title: params[:diary][:title], message: params[:diary][:message])
+        diary = Diary.new(title: params[:diary][:title],message: params[:diary][:message])
         diary.save
         redirect_to root_path
+    end
+    
+    def get_image
+        diary = Diary.find(params[:id]) #↓詳細は説明を後述する
+        send_data image.file, disposition: :inline, type: 'image/jpg'
+    end
+    
+    def destroy
+        diary = Diary.find(params[:id])
+        diary.destroy
+        redirect_to root_path
+    end
+    
+    def edit
+        @diary = Diary.find(params[:id])
+    end
+    
+    def update
+        diary = Diary.find(params[:id])
+        diary.update(title: params[:diary][:title], message: params[:diary][:message])
+        redirect_to root_path
+    
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'diaries/index', to: 'diaries#index'
-  get 'diaries/new', to: 'diaries#new'
+
   resources :users
   resources :diaries
+  delete 'diaries/:id', to: 'diaries#destroy'
+  get 'diaries/:id/edit', to: 'diaries#edit'
+  patch 'diaries/:id', to: 'diaries#update'
+  get 'get_image/:id', to: 'diaries#get_image'
   root 'diaries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
