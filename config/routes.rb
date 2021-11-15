710f52693a87beb91bda09b2836a6458c6b38e33
Chisato Morita <1911140002c@ed.fuk.kindai.ac.jp>
2021-11-15 07:00:34 +0000

    CRUD機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   
   resources :users
   resources :diaries
-  delete 'diaries/:id', to: 'diaries#destroy'
-  get 'diaries/:id/edit', to: 'diaries#edit'
-  patch 'diaries/:id', to: 'diaries#update'
   get 'get_image/:id', to: 'diaries#get_image'
   root 'diaries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
