b0605e1966904828fa2ca435bdab0436f1ad2c11
Ubuntu <ubuntu@ip-172-31-21-56.ec2.internal>
2021-11-29 07:08:31 +0000

    validation

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -10,34 +10,20 @@ class DiariesController < ApplicationController
     end
     
     def create
-<<<<<<< HEAD
         
         if  params[:diary][:file].present?
             file = params[:diary][:file].read
             @diary = Diary.new(title: params[:diary][:title], message: params[:diary][:message],
-            file: file, date: Time.current)
+            file: file, date: Time.current, qid: params[:diary][:qid])
             if @diary.save
                 flash[:notice] = '1レコード追加しました'
                 redirect_to diaries_path
             else
                 render 'new'
             end
-            
         else
             @diary = Diary.new(title: params[:diary][:title], message: params[:diary][:message],
-            date: Time.current)
-=======
-        if  params[:diary][:file].present?
-            file = params[:diary][:file].read
-            diary = Diary.new(title: params[:diary][:title], message: params[:diary][:message],
-            file: file, date: Time.current, qid: params[:diary][:qid])
-            diary.save
-            redirect_to diaries_path
-        else
-            diary = Diary.new(title: params[:diary][:title], message: params[:diary][:message],
             date: Time.current, qid: params[:diary][:qid])
->>>>>>> 5aba0580b92a949914d0eca61adead18434bc5d5
-            
             if @diary.save
                 flash[:notice] = '1レコード追加しました'
                 redirect_to diaries_path
